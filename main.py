3d214f0d5f109f662f149715c0cf1984e2e1578f
ValeryGnj <valerygnj@gmail.com>
2023-09-24 16:39:46 +0200

    fill_all_comments_for_script

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ def main():
     wb = openpyxl.load_workbook('apartment_rental_agreement.xlsx', data_only=True)
     # получаем лист с данными
     sheet = wb['Список_клиентов']
-
+    # генерируем список заголовков столбцов с данными
     header_list = [item.value for row in sheet['B1':'AQ1'] for item in row if
                    item.value is not None and item.value != " "]
+    # генерируем список данных из строки таблицы
     data_list = [item.value for row in sheet['B2':'AQ2'] for item in row if
                  item.value is not None and item.value != " "]
+    # открываем шаблон в word для заполнения данными из excel
     doc = DocxTemplate('template_rent_by_flat.docx')
+
+    # присваиваем ключам и значениям соответствующие списки с данными
     key = header_list
     value = data_list
+    # генерируем словарь по ключам и значениям из списков сгенерированных выше
     a = {k: v for k, v in zip(key, value)}
+
+    # заполняем шаблон данными из словаря
     context = a
     doc.render(context)
+    # сохраняем готовый документ
     doc.save('result_for_rent.docx')
 
 
